79c0b98fab4e179bbfa39dd9d8194ea9a033ea1b
Felipe Lecot <lecotfelipe@gmail.com>
2025-03-14 07:01:01 -0300

    Use port as env marking

--- src/utils/generateSitemap.ts
+++ src/utils/generateSitemap.ts
@@ -2,7 +2,7 @@ import getValidPaths from "../utils/getValidPaths";
 import path from "path";
 
 const generateSitemap = async () => {
-  const contentPath = path.join(process.env.NODE_ENV === 'production' ? "dist" : "src", "content");
+  const contentPath = path.join(process.env.PORT ? "dist" : "src", "content");
   const paths = await getValidPaths(contentPath);
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">

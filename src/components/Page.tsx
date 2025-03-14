fcfbbf3e78a8d11ba74d2b41410d1be410f401cc
Felipe Lecot <lecotfelipe@gmail.com>
2025-03-14 06:05:21 -0300

    Delete test react and bundle

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -5,12 +5,9 @@ interface PageProps {
 }
 
 export const Page: React.FC<PageProps> = ({ content }) => {
-  const [state, setState] = React.useState(0);
   return (
     <div>
       <h1>SSR Pages</h1>
-      <p>{state}</p>
-      <button onClick={() => setState(state + 1)}>Click me</button>
       <div id='content' dangerouslySetInnerHTML={{ __html: content }} />
     </div>
   );
